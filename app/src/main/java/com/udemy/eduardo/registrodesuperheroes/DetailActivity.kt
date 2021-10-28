506ed813f356de823612382893d70fbd47dd94ca
Eduardo Lara Ramirez <eduardo.larar@sat.gob.mx>
2021-10-28 16:04:04 -0500

    Explicit intentes y comunicación entre actividades lista

--- app/src/main/java/com/udemy/eduardo/registrodesuperheroes/DetailActivity.kt
+++ app/src/main/java/com/udemy/eduardo/registrodesuperheroes/DetailActivity.kt
@@ -2,10 +2,23 @@ package com.udemy.eduardo.registrodesuperheroes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.udemy.eduardo.registrodesuperheroes.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bundle = intent.extras!!
+        val superHeroeName = bundle.getString(MainActivity.SUPERHEROE_NAME_KEY) ?: ""
+        val alterEgo = bundle.getString(MainActivity.ALTER_EGO_KEY) ?: ""
+        val bio = bundle.getString(MainActivity.BIO_KEY) ?: ""
+        val power = bundle.getFloat(MainActivity.POWER_KEY)
+
+        binding.heroName.text = superHeroeName
+        binding.alterEgo.text = alterEgo
+        binding.bio.text = bio
+        binding.ratingBar.rating = power
     }
 }
--- app/src/main/java/com/udemy/eduardo/registrodesuperheroes/MainActivity.kt
+++ app/src/main/java/com/udemy/eduardo/registrodesuperheroes/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.udemy.eduardo.registrodesuperheroes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.udemy.eduardo.registrodesuperheroes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val SUPERHEROE_NAME_KEY = "superheroe_name"
+        const val ALTER_EGO_KEY = "alter_ego"
+        const val BIO_KEY = "bio"
+        const val POWER_KEY = "power"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.saveButton.setOnClickListener {
+            val superHeroeName = binding.heroNameEdit.text.toString()
+            val alterEgo = binding.alterEgoEdit.text.toString()
+            val bio = binding.bioEdit.text.toString()
+            val power = binding.rattingBar.rating
+            openDetailActivity(superHeroeName, alterEgo, bio, power)
+        }
+    }
+
+    private fun openDetailActivity(superHeroeName: String, alterEgo: String,
+                                    bio: String, power: Float) {
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra(SUPERHEROE_NAME_KEY, superHeroeName)
+        intent.putExtra(ALTER_EGO_KEY, alterEgo)
+        intent.putExtra(BIO_KEY, bio)
+        intent.putExtra(POWER_KEY, power)
+        startActivity(intent)
     }
 }
